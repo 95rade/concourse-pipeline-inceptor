358116d9e4c14d097548e1a0975c1b32e6eca18e
Luciano Silva <lsilva@pivotal.io>
2016-03-16 18:32:12 -0400

    Update

--- ci/tools/update_pipeline.sh
+++ ci/tools/update_pipeline.sh
@@ -7,6 +7,6 @@ echo "Pushing pipeline.yml to Concourse server"
 # ls -la pipeline
 
 # fly -t $2 sync
-fly -t local login -c $1 -u $2 -p $3
+fly -t local login -c $1 #-u $2 -p $3
 fly -t local pipelines
 fly -t local set-pipeline -n -p $4 -c pipeline/pipeline.yml -l $5
